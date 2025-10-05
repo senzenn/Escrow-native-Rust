992f2d9a095321d1e7e353671cb0cbf8a1335813
senzenn <ascenzen11@gmail.com>
2025-10-06 00:25:53 +0530

    send sol function

--- src/processor.rs
+++ src/processor.rs
@@ -18,15 +18,49 @@ use crate::{error::EscrowError, instructions::VaultInstruction, state::LockAccou
 pub struct Processor;
 
 impl Processor {
-    pub fn process(program_id: &Pubkey, account_info: &AccountInfo) -> ProgramResult {
-        Ok(())
+    pub fn process(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let instruction = VaultInstruction::unpack(instruction_data)?;
+        match instruction {
+            VaultInstruction::SendSol { amount, lottery_id } => {
+                msg!("Instruction Send Sol ");
+                //TODO: main logic
+            }
+            VaultInstruction::Cancel { lottery_id } => {
+                msg!("Instruction: Cancel");
+                //TODO: main logic
+            }
+            VaultInstruction::CloseVault { lottery_id } => {
+                msg!("Instuction Close");
+                //TODO: main logic
+            }
+        }
+    }
+
+    // process send sol function defination
+
+    fn process_send_sol(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        amount: u64,
+        lottery_id: &[u8],
+    ) {
+        let account_info_iter = &mut accounts.iter();
+        let buyer = next_account_info(account_info_iter)?;
+        let vault = next_account_info(account_info_iter)?;
+        let system_program = next_account_info(account_info_iter)?;
+
+        // validate buyer is signer important validator
+        if !buyer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        // PDA
+
+        let vault_seed = b"vault";
+        let seed = &[vault_seed.as_ref(), buyer.key.as_ref(), lottery_id];
     }
 }
-
-//pub fn process_instruction(
-//    _program_id: &Pubkey,
-//    accounts: &[AccountInfo],
-//    _instruction_data: &[u8],
-//) -> ProgramResult {
-//    Ok(())
-//}
